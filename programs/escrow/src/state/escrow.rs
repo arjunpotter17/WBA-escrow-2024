6c082569a6c12585b59633a922d24f014b2216b6
Arjun Mohan <arjunpotter17@gmail.com>
2024-05-29 16:55:21 +0530

    Update escrow state

--- programs/escrow/src/state/escrow.rs
+++ programs/escrow/src/state/escrow.rs
@@ -4,9 +4,9 @@ use anchor_lang::prelude::*;
 pub struct Escrow {
     pub seed: u64,
     pub maker: Pubkey,
-    pub mint_a: Pubkey,
-    pub mint_b: Pubkey,
-    pub receive: u64,
+    pub mint_account_a: Pubkey,
+    pub mint_account_b: Pubkey,
+    pub amount_a: u64,
     pub bump: u8,
 }
 
